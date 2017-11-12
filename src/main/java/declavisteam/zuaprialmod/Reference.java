a122c5b044deec50cac50f6e0e582a970ff48b78
Alexander de Clavis <declavis.company@gmail.com>
2017-11-12 21:15:08 +0100

    Progresses to an first Startup

---
+++ src/main/java/declavisteam/zuaprialmod/Reference.java
@@ -0,0 +1,10 @@
+package declavisteam.zuaprialmod;
+
+public class Reference {
+	public static final String MODID = "zm";
+	public static final String NAME = "Zuaprial Mod";
+	public static final String VERSION = "0.1 - MC 1.12.2";
+	
+	public static final String CLIENTPROXY = "declavisteam.zuaprialmod.proxy.ClientProxy";
+	public static final String COMMONPROXY = "declavisteam.zuaprialmod.proxy.CommonProxy";
+}
---
+++ src/main/java/declavisteam/zuaprialmod/handlers/RegistryHandler.java
@@ -0,0 +1,10 @@
+package declavisteam.zuaprialmod.handlers;
+
+public class RegistryHandler {
+
+	public static void Client()
+	{}
+	
+	public static void Common()
+	{}
+}
